1c00f3482d6f71868114d0cf7d1e0bb6480f959f
Igroshka <86719527+Igroshka@users.noreply.github.com>
2024-06-09 18:58:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/api/drawing.ts
@@ -0,0 +1,21 @@
+import { PluginErrorType, createErrorResponse } from '@lobehub/chat-plugin-sdk';
+
+import { RequestData } from '@/type';
+
+export const config = {
+  runtime: 'edge',
+};
+
+const BASE_URL = 'https://image.pollinations.ai/prompt';
+
+export default async (req: Request) => {
+  if (req.method !== 'POST') return createErrorResponse(PluginErrorType.MethodNotAllowed);
+
+  const { description, params } = (await req.json()) as RequestData;
+
+  const result = `![result](${BASE_URL}/${description
+    .replaceAll(/\s/g, '%20')
+    .replaceAll(/[,.]/g, '')}?${params})`;
+
+  return new Response(result);
+};
