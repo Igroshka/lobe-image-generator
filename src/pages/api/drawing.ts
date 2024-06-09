88ad4c19b4e341312ebe8ea202f48dcdfe21dd39
Igroshka <86719527+Igroshka@users.noreply.github.com>
2024-06-10 02:20:55 +0700

    Update drawing.ts

--- src/pages/api/drawing.ts
+++ src/pages/api/drawing.ts
@@ -6,22 +6,20 @@ export const config = {
   runtime: 'edge',
 };
 
-// Загружаем пайплайн для генерации изображений из модели
-const generator = await pipeline('text-to-image', 'openskyml/dalle-3-xl'); 
-
 export default async (req: Request) => {
   if (req.method !== 'POST') return createErrorResponse(PluginErrorType.MethodNotAllowed);
 
-  const { description } = (await req.json()) as RequestData; // Удалил 'params'
+  const { description } = (await req.json()) as RequestData;
 
   try {
-    // Генерируем изображение с помощью модели
+    // Динамически импортируем и загружаем модель внутри функции
+    const generator = await pipeline('text-to-image', 'openskyml/dalle-3-xl'); 
+
     const image = await generator(description, { 
-      // Дополнительные параметры модели (если необходимо)
+      // Дополнительные параметры модели 
     });
 
-    // Конвертируем изображение в формат, подходящий для отправки в ответе 
-    const imageBuffer = await image.toBuffer('png'); // Или другой формат, например 'jpeg'
+    const imageBuffer = await image.toBuffer('png'); 
     const imageBase64 = imageBuffer.toString('base64');
 
     const result = `![result](data:image/png;base64,${imageBase64})`;
